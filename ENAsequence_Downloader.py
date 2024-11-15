de0b2175ba32a2b466e08c22c2eda36c12715705
CHAU VINH <chauvinhtth13@gmail.com>
2024-11-15 16:28:14 +0700

    1.1.1

--- ENAsequence_Downloader.py
+++ ENAsequence_Downloader.py
@@ -218,7 +218,7 @@ if __name__ == "__main__":
                 else:
                     print(f"ERROR: '{links_txt_file}' not found!")
         else:
-            print("Thank you. Bye!! ")
+            print("Thank you. Goodbye :)")
             sys.exit(0)
     else:
         parser.print_help()
